58b4b8331edc1e81073ba29ce7a94f9508b3202b
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-04-28 15:33:00 +0700

    Add api/get-posts.js

---
+++ api/get-posts.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_DATABASE_URL
+    });
+}
+
+const db = admin.database();
+
+module.exports = async (req, res) => {
+    const snapshot = await db.ref('posts').orderByChild('timestamp').once('value');
+    const posts = [];
+    snapshot.forEach(child => {
+        posts.push({ id: child.key, ...child.val() });
+    });
+    posts.reverse();
+    res.status(200).json({ posts });
+};
